22fc632e2dfa2d815e091f082fb1302ccaf867a9
Marco <Marco@DESKTOP-3GOQEE8>
2021-06-17 14:13:16 -0300

    Add project files.

---
+++ Setimo/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+
+namespace Setimo
+{
+    class Conta
+    {
+        public int Numero { get; private set; } // Auto Properties
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nome) // Used when there is no initial deposit
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public Conta(int numero, string nome, double saldo)
+        {
+            Numero = numero;
+            Nome = nome;
+            Saldo = saldo;
+        }
+        public void CriarConta(int numero, string nome, double saldo)
+        {
+            Numero = numero;
+            Nome = nome;
+            Saldo = saldo;
+
+        }
+        public void Deposito(double saldo)
+        {
+            Saldo += saldo;
+        }
+
+        public void retirada(double saldo)
+        {
+            Saldo = Saldo - saldo - 5.00; // The bank charges an absurd fee of US$5.00 for each withdrawal!
+        }
+
+        public override string ToString() // Define the output string to use in console.writeline (c)
+        {
+            return
+            "Conta: " + Numero + " , Titular: " + Nome + ", Saldo: $ "
+            + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ Setimo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Setimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            Console.Write("Entre o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string havera = Console.ReadLine();
+            if (havera == "s")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                valor = 0;
+            }
+            Conta c = new Conta(conta, nome, valor);
+
+            c.CriarConta(conta, nome, valor);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para depósito: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(valor);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.retirada(valor);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(c);
+
+        }
+    }
+}
